8a1f5dd6b9260e90701ed3ff9046fe631181140d
Timo Lambing <timo@skillhub.ee>
2024-11-07 08:08:38 +0000

    Oauth working 07.11.2024

--- client/src/components/common/Sidebar.tsx
+++ client/src/components/common/Sidebar.tsx
@@ -1,21 +1,43 @@
 // client/src/components/common/Sidebar.tsx
 
-import React, { useState, useLayoutEffect, useEffect } from 'react'
-import { Page, Panel, Block, BlockTitle, List, ListInput, Button, Dialog, Notification } from 'konsta/react'
-import { TonConnectButton } from '@tonconnect/ui-react'
+import React, { useState, useLayoutEffect, useEffect, useRef } from 'react'
+import { Page, Panel, Block, BlockTitle, List, ListInput, Button, Dialog, Notification, Popover } from 'konsta/react'
 import { useNavigate } from 'react-router-dom'
 import useUserStore from '../../store/useUserStore'
 import useSessionStore from '../../store/useSessionStore'
 import Lottie from 'react-lottie'
 import bunnyAnimationData from '../../animations/bunny.json'
+import { TbBrandX } from 'react-icons/tb' // Import the X.com icon
+// Removed axiosInstance import
+// import axiosInstance from '../../api/axiosInstance'
 
 const Sidebar: React.FC = () => {
     const navigate = useNavigate()
 
-    const { roles, sidebarOpened, toggleSidebar } = useUserStore((state) => ({
+    const {
+        roles,
+        sidebarOpened,
+        toggleSidebar,
+        twitterAuthenticated,
+        twitterUsername,
+        setTwitterAuthenticated,
+        setTwitterUserData,
+        fetchTwitterAuthStatus,
+        removeTwitterAccount,
+        authenticated,
+        telegramUserId // Add this line
+    } = useUserStore((state) => ({
         roles: state.roles,
         sidebarOpened: state.sidebarOpened,
-        toggleSidebar: state.toggleSidebar
+        toggleSidebar: state.toggleSidebar,
+        twitterAuthenticated: state.twitterAuthenticated,
+        twitterUsername: state.twitterUsername,
+        setTwitterAuthenticated: state.setTwitterAuthenticated,
+        setTwitterUserData: state.setTwitterUserData,
+        fetchTwitterAuthStatus: state.fetchTwitterAuthStatus,
+        removeTwitterAccount: state.removeTwitterAccount,
+        authenticated: state.authenticated, // Add this line
+        telegramUserId: state.telegramUserId?.toString() // Add this line
     }))
 
     const { erc20WalletAddress, solanaWalletAddress, tonWalletAddress, updateWalletAddresses } = useUserStore((state) => ({
@@ -25,11 +47,8 @@ const Sidebar: React.FC = () => {
         updateWalletAddresses: state.updateWalletAddresses
     }))
 
-    const { theme, darkMode, setTheme, setColorTheme, setDarkMode } = useSessionStore((state) => ({
-        theme: state.theme,
+    const { darkMode, setDarkMode } = useSessionStore((state) => ({
         darkMode: state.darkMode,
-        setTheme: state.setTheme,
-        setColorTheme: state.setColorTheme,
         setDarkMode: state.setDarkMode
     }))
 
@@ -164,98 +183,197 @@ const Sidebar: React.FC = () => {
         return buttons
     }
 
+    // Popover state
+    const [popoverOpen, setPopoverOpen] = useState(false)
+    const popoverTargetRef = useRef<HTMLButtonElement>(null)
+
+    // Function to handle authentication
+    const handleAuthenticate = () => {
+        if (!authenticated || !telegramUserId) {
+            setNotificationText('Please log in to authenticate with X.')
+            setNotificationOpen(true)
+            return
+        }
+
+        const returnTo = encodeURIComponent(window.location.href)
+        const authUrl = `${import.meta.env.VITE_API_BASE_URL}/api/auth/twitter/start?returnTo=${returnTo}&telegramUserId=${telegramUserId}`
+
+        window.open(authUrl, '_blank')
+        setPopoverOpen(false)
+    }
+
+    // Function to handle removal of Twitter account
+    const handleRemoveTwitterAccount = async () => {
+        const message = await removeTwitterAccount()
+        setNotificationText(message)
+        setNotificationOpen(true)
+        setPopoverOpen(false)
+    }
+
+    // Fetch Twitter authentication status
+    useEffect(() => {
+        const fetchTwitterStatus = async () => {
+            try {
+                await fetchTwitterAuthStatus()
+            } catch (error) {
+                console.error('Error fetching Twitter status:', error)
+            }
+        }
+
+        fetchTwitterStatus()
+    }, [fetchTwitterAuthStatus])
+
+    // Handle Twitter authentication callback
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search)
+        const twitterAuthStatus = urlParams.get('twitterAuth')
+        if (twitterAuthStatus === 'success') {
+            setTwitterAuthenticated(true)
+            setNotificationText('X authentication successful!')
+            setNotificationOpen(true)
+            // Remove the query parameter from the URL
+            navigate('', { replace: true })
+        } else if (twitterAuthStatus === 'failure') {
+            setNotificationText('X authentication failed. Please try again.')
+            setNotificationOpen(true)
+            navigate('', { replace: true })
+        }
+    }, [setTwitterAuthenticated, navigate])
+
     return (
         <Panel side="right" floating opened={sidebarOpened} onBackdropClick={toggleSidebar}>
-            <Page>
-                <Block className="space-y-4">
-                    <BlockTitle className="mb-1">Connect your TON Wallet</BlockTitle>
-                    <TonConnectButton className="mx-auto" />
+            <Page className="flex flex-col h-full">
+                <Block className="space-y-4 flex-grow !items-center  !flex !flex-col">
+                    <BlockTitle className="!text-center mb-1 !m-0 !p-0">Your Wallet</BlockTitle>
+                    {/* <TonConnectButton className="mx-auto" /> */}
 
-                    <Block className="space-y-2">{renderRoleBasedButtons()}</Block>
+                    <div
+                        className="relative overflow-hidden rounded-2xl shadow-lg m-0 tab-background mb-4 cursor-pointer"
+                        onClick={() => setWalletDialogOpen(true)}
+                    >
+                        <div className="relative z-10 m-[2px] rounded-2xl tab-content p-4">
+                            <div className="text-white text-center">
+                                <h2 className="text-lg font-bold mb-2">Where will we distribute rewards?</h2>
+                                <p className="text-sm">Add wallet addresses!</p>
+                            </div>
+                        </div>
+                    </div>
 
-                    {/* New Banner with Flashy Design */}
-                    <Block>
-                        <div
-                            className="relative overflow-hidden rounded-2xl shadow-lg m-0 tab-background mb-4 cursor-pointer"
-                            onClick={() => setWalletDialogOpen(true)}
+                    {/* Authenticated X User */}
+                    <BlockTitle className="!text-center !mt-10">Authenticated X User</BlockTitle>
+                    <div className="flex justify-center relative">
+                        {/* Custom Twitter Button */}
+                        <button
+                            ref={popoverTargetRef}
+                            className="flex items-center space-x-2 px-4 py-2 rounded-full bg-gradient-to-r from-blue-500 to-purple-600 text-white font-semibold shadow-lg hover:shadow-xl transition-transform transform hover:scale-105"
+                            onClick={() => {
+                                setPopoverOpen(true)
+                            }}
                         >
-                            <div className="relative z-10 m-[2px] rounded-2xl tab-content p-4">
-                                <div className="text-white text-center">
-                                    <h2 className="text-lg font-bold mb-2">Where will we distribute rewards?</h2>
-                                    <p className="text-sm">Add wallet addresses!</p>
-                                </div>
-                            </div>
-                        </div>
-                    </Block>
+                            <TbBrandX className="w-5 h-5" />
+                            <span className="text-sm">{twitterAuthenticated ? `@${twitterUsername}` : 'Authenticate with X'}</span>
+                        </button>
 
-                    {/* Wallet Addresses Dialog */}
-                    <Dialog opened={walletDialogOpen} onBackdropClick={() => setWalletDialogOpen(false)} className="!m-0 !p-0 !rounded-2xl !bg-opacity-80">
-                        <div className="p-0 relative">
-                            {/* Close Button */}
-                            <button className="absolute right-1 text-gray-500 hover:text-gray-700" onClick={() => setWalletDialogOpen(false)}>
-                                &times;
-                            </button>
-                            {/* Bunny Animation */}
-                            <div className="flex items-center justify-center mb-4">
-                                <Lottie options={bunnyAnimationOptions} height={150} width={150} />
-                            </div>
-                            {/* Heading */}
-                            <div className="text-md font-bold text-center mt-4">Add wallet addresses</div>
-                            {/* Text */}
-                            <p className="text-center mt-2 px-4">Add your wallet addresses for potential reward distributions:</p>
-                            {/* Input Fields */}
-                            <List className="!m-0 !p-4">
-                                <ListInput
-                                    outline
-                                    label="ERC-20"
-                                    placeholder="Enter your ERC-20 address"
-                                    value={walletAddresses.erc20}
-                                    onChange={(e) => setWalletAddresses({ ...walletAddresses, erc20: e.target.value })}
-                                />
-                                <ListInput
-                                    outline
-                                    label="Solana"
-                                    placeholder="Enter your Solana address"
-                                    value={walletAddresses.solana}
-                                    onChange={(e) => setWalletAddresses({ ...walletAddresses, solana: e.target.value })}
-                                />
-                                <ListInput
-                                    outline
-                                    label="TON"
-                                    placeholder="Enter your TON address"
-                                    value={walletAddresses.ton}
-                                    onChange={(e) => setWalletAddresses({ ...walletAddresses, ton: e.target.value })}
-                                />
-                            </List>
-                            {/* Save Button */}
-                            <div className="flex justify-center mt-4 mb-4">
-                                <Button
-                                    outline
-                                    rounded
-                                    onClick={handleSaveWalletAddresses}
-                                    className="!text-xs mt-4 font-bold shadow-xl min-w-28 !mx-auto !h-7"
-                                    style={{
-                                        background: 'linear-gradient(to left, #ff0077, #7700ff)',
-                                        color: '#fff',
-                                        borderColor: '#9c27b0'
-                                    }}
-                                >
-                                    Save
-                                </Button>
-                            </div>
-                        </div>
-                    </Dialog>
-
-                    {/* Success Notification */}
-                    <Notification
-                        className="fixed top-12 left-0 z-50 border"
-                        opened={notificationOpen}
-                        title="Success"
-                        text={notificationText}
-                        button={<Button onClick={() => setNotificationOpen(false)}>Close</Button>}
-                        onClose={() => setNotificationOpen(false)}
-                    />
+                        {/* Popover */}
+                        <Popover
+                            opened={popoverOpen}
+                            target={popoverTargetRef.current}
+                            onBackdropClick={() => setPopoverOpen(false)}
+                            size="w-56"
+                            className="right-0"
+                            style={{ transform: 'translateX(-50%)' }}
+                        >
+                            <Block>
+                                {twitterAuthenticated ? (
+                                    <button
+                                        className="w-full px-4 py-2 rounded-full bg-red-500 text-white font-semibold shadow-md hover:bg-red-600 transition-colors"
+                                        onClick={handleRemoveTwitterAccount}
+                                    >
+                                        Remove X User
+                                    </button>
+                                ) : (
+                                    <button
+                                        className="w-full px-4 py-2 rounded-full bg-blue-500 text-white font-semibold shadow-md hover:bg-blue-600 transition-colors"
+                                        onClick={handleAuthenticate}
+                                    >
+                                        Authenticate
+                                    </button>
+                                )}
+                            </Block>
+                        </Popover>
+                    </div>
                 </Block>
+
+                {/* Move role-based buttons to the bottom */}
+                <Block className="space-y-2 mb-20">{renderRoleBasedButtons()}</Block>
+
+                {/* Wallet Addresses Dialog */}
+                <Dialog opened={walletDialogOpen} onBackdropClick={() => setWalletDialogOpen(false)} className="!m-0 !p-0 !rounded-2xl !bg-opacity-80">
+                    <div className="p-0 relative">
+                        {/* Close Button */}
+                        <button className="absolute right-1 text-gray-500 hover:text-gray-700" onClick={() => setWalletDialogOpen(false)}>
+                            &times;
+                        </button>
+                        {/* Bunny Animation */}
+                        <div className="flex items-center justify-center mb-4">
+                            <Lottie options={bunnyAnimationOptions} height={150} width={150} />
+                        </div>
+                        {/* Heading */}
+                        <div className="text-md font-bold text-center mt-4">Add wallet addresses</div>
+                        {/* Text */}
+                        <p className="text-center mt-2 px-4">Add your wallet addresses for potential reward distributions:</p>
+                        {/* Input Fields */}
+                        <List className="!m-0 !p-4">
+                            <ListInput
+                                outline
+                                label="ERC-20"
+                                placeholder="Enter your ERC-20 address"
+                                value={walletAddresses.erc20}
+                                onChange={(e) => setWalletAddresses({ ...walletAddresses, erc20: e.target.value })}
+                            />
+                            <ListInput
+                                outline
+                                label="Solana"
+                                placeholder="Enter your Solana address"
+                                value={walletAddresses.solana}
+                                onChange={(e) => setWalletAddresses({ ...walletAddresses, solana: e.target.value })}
+                            />
+                            <ListInput
+                                outline
+                                label="TON"
+                                placeholder="Enter your TON address"
+                                value={walletAddresses.ton}
+                                onChange={(e) => setWalletAddresses({ ...walletAddresses, ton: e.target.value })}
+                            />
+                        </List>
+                        {/* Save Button */}
+                        <div className="flex justify-center mt-4 mb-4">
+                            <Button
+                                outline
+                                rounded
+                                onClick={handleSaveWalletAddresses}
+                                className="!text-xs mt-4 font-bold shadow-xl min-w-28 !mx-auto !h-7"
+                                style={{
+                                    background: 'linear-gradient(to left, #ff0077, #7700ff)',
+                                    color: '#fff',
+                                    borderColor: '#9c27b0'
+                                }}
+                            >
+                                Save
+                            </Button>
+                        </div>
+                    </div>
+                </Dialog>
+
+                {/* Success Notification */}
+                <Notification
+                    className="fixed top-12 left-0 z-50 border"
+                    opened={notificationOpen}
+                    title="Message"
+                    text={notificationText}
+                    button={<Button onClick={() => setNotificationOpen(false)}>Close</Button>}
+                    onClose={() => setNotificationOpen(false)}
+                />
             </Page>
         </Panel>
     )
