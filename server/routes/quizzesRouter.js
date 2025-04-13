19ca28e2493d49aa9ef31b3778f989154cb2f448
kyyasilmyradovv <kyyas.ilmyrdov@gmail.com>
2025-04-14 01:32:06 +0500

    submit logic update

--- server/routes/quizzesRouter.js
+++ server/routes/quizzesRouter.js
@@ -2,7 +2,6 @@ const express = require('express');
 const {
   getQuestions,
   submitAnswer,
-  finishQuiz,
 } = require('../controllers/quizControllers');
 const { protectForUser } = require('../controllers/userAuthControllers');
 const { checkInputs } = require('../middleware/checkInputs');
@@ -11,6 +10,5 @@ const router = express.Router();
 router.use(protectForUser);
 router.get('/', getQuestions);
 router.post('/submit', checkInputs('submit_quiz', 'create'), submitAnswer);
-router.post('/finish', finishQuiz);
 
 module.exports = router;
