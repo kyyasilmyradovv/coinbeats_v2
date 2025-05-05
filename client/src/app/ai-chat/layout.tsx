4d8ecd77685b4cc6d691a071fda79cdc0deaea36
kyyasilmyradovv <kyyas.ilmyrdov@gmail.com>
2025-05-06 00:15:48 +0500

    wallet button and sidebar compnent fixed

--- client/src/app/ai-chat/layout.tsx
+++ client/src/app/ai-chat/layout.tsx
@@ -4,12 +4,14 @@ import { ChatSidebar } from '@/components/chatSidebar'
 import dynamic from 'next/dynamic'
 import { useState } from 'react'
 import { Button } from '@/components/ui/button'
+import { useIsMobile } from '@/hooks/use-mobile'
 import { Wallet } from 'lucide-react'
 
 const WalletSidebar = dynamic(() => import('../../components/wallet'), { ssr: false })
 
 export default function AIChat({ children }: { children: React.ReactNode }) {
     const [showWalletSidebar, setShowWalletSidebar] = useState(false)
+    const isMobile = useIsMobile()
 
     const toggleWalletSidebar = () => {
         setShowWalletSidebar(!showWalletSidebar)
@@ -22,11 +24,13 @@ export default function AIChat({ children }: { children: React.ReactNode }) {
     return (
         <SidebarProvider>
             <ChatSidebar />
-            <div className="fixed top-16.5 left-0 z-10">
-                <Button variant={showWalletSidebar ? 'secondary' : 'ghost'} className="p-2" size="icon">
-                    <SidebarTrigger className="h-5 w-5" />
-                </Button>
-            </div>
+            {isMobile && (
+                <div className="fixed top-16.5 left-0 z-10">
+                    <Button variant={showWalletSidebar ? 'secondary' : 'ghost'} className="p-2" size="icon">
+                        <SidebarTrigger className="h-5 w-5" />
+                    </Button>
+                </div>
+            )}
             <div className="fixed top-16.5 right-0 z-10">
                 <Button variant={showWalletSidebar ? 'secondary' : 'ghost'} onClick={toggleWalletSidebar} className="p-2" size="icon">
                     <Wallet className="h-5 w-5" />
